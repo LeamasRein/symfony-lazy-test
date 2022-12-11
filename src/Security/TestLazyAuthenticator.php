e58394e73f925bf7c636164b2a5b3188bfd84e64
roman.e <roman.e@finanso.com>
2022-12-11 13:05:41 +0300

    test lazy auth

---
+++ src/Security/TestLazyAuthenticator.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Security;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+
+class TestLazyAuthenticator extends AbstractAuthenticator
+{
+    public function supports(Request $request): ?bool
+    {
+        throw new Exception("Lazy not working");
+        return false;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return null;
+    }
+}
